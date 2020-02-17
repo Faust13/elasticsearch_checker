f968c6389e5c8ba666b0fdbf35c3df39067ccc11
Vlad Bubnov <vldbubnov@gmail.com>
2020-02-17 12:56:45 +0300

    change directory structure

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import config as conf
-from verification import check_time, check_url
-import rules
+import yaml
+from jinja2 import Environment, FileSystemLoader
 
 if __name__ == '__main__':
+    config_data = yaml.load(open('./conf/rules.yml'))
+    print(config_data)
+    env = Environment(loader = FileSystemLoader('./templates'),   trim_blocks=True, lstrip_blocks=True)
+    template = env.get_template('rules.py.j2')
+    rules_py = template.render(config_data)
+    with open("rules.py", "w") as fh:
+        fh.write(rules_py)
+
+    import rules
     tl.start(block=True)
