e53ec2bb5c05a13320c1d6014c680bedd5151f53
Vlad Bubnov <vldbubnov@gmail.com>
2020-02-17 12:11:08 +0300

    move application logic to template

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import config as conf
 from verification import check_time, check_url
-
+import rules
 
 if __name__ == '__main__':
-    time = conf.STATS_FOR
-    check_time(time)
-    url = check_url(conf.ES_HOST, conf.ES_PORT, alert.ES_INDEX)
-    import rules
     tl.start(block=True)
